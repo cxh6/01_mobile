516d76057d4c11ac69f93462ba5c4c8f2ae527b7
cxh <564668501@qq.com>
2020-02-12 23:01:55 +0800

    完成layout组件

---
+++ src/views/layout/index.vue
@@ -0,0 +1,42 @@
+<template>
+  <div class="container">
+    <!-- 导航栏 -->
+    <van-nav-bar fixed title="黑马头条" right-text="搜索" @click-right="$router.push('/search')" />
+    <!-- 中间区域 -->
+    <div class="my-wrapper">
+      <router-view></router-view>
+    </div>
+    <!-- 标签栏 -->
+    <van-tabbar route>
+      <van-tabbar-item to="/home" icon="home-o">首页</van-tabbar-item>
+      <van-tabbar-item to="/question" icon="chat-o">问答</van-tabbar-item>
+      <van-tabbar-item to="/video" icon="video-o">视频</van-tabbar-item>
+      <van-tabbar-item to="/user" icon="user-o">我的</van-tabbar-item>
+    </van-tabbar>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'layout-index'
+}
+</script>
+
+<style scoped lang='less'>
+.container {
+  width: 100%;
+  height: 100%;
+  position: relative;
+  .my-wrapper {
+    width: 100%;
+    height: 100%;
+    overflow: hidden;
+    padding-top: 92px;
+    padding-bottom: 100px;
+    box-sizing: border-box;
+    &.noTop {
+      padding-top: 0;
+    }
+  }
+}
+</style>
