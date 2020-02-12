01c92eb3e3b64530aa9eff6f0f5f5f77362ff50b
cxh <564668501@qq.com>
2020-02-12 22:44:25 +0800

    配置路由

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,5 @@
 <template>
   <div id="app">
-    <van-button type="default">默认按钮</van-button>
-    <van-button type="primary">主要按钮</van-button>
-    <van-button type="info">信息按钮</van-button>
-    <van-button type="warning">警告按钮</van-button>
-    <van-button type="danger">危险按钮</van-button>
-    <p class="a"></p>
     <router-view />
   </div>
 </template>
@@ -17,9 +11,5 @@ export default {
 </script>
 
 <style lang="less">
-.a {
-  width: 375px;
-  height: 50px;
-  background-color: #ccc;
-}
+
 </style>
